31452dde69ee81329b0ce1a636c82bc473380e80
fabrice-bazire <bazire.fabrice@gmail.com>
2020-09-11 15:55:36 +0200

    ajout du gitignore + TD1 integration css

--- python web/td4/exo3.py
+++ python web/td4/exo3.py
@@ -8,9 +8,12 @@ for q in questions :
     vote = q.find("span", class_="vote-count-post")
     reponses = q.find("div", class_=(["status answered", "status answered-accepted"]))
     r = reponses.find("strong")
+    rep = list()
     for quest in question : 
-        print(quest.text)
-        print("nombre de vote(s) : ", vote.text)
-        print("nombre de réponse(s) : ", r.text)
-        print()
+        res = list()
+        res.append(quest.text)
+        res.append(vote.text)
+        res.append(r.text)
+        rep.append(res)
+print(rep)
         
