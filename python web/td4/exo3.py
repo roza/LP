aabd05687c0df5778312594745b5804c2ec17e55
Fabrice Bazire <fbazire@info03-10.iut45.univ-orleans.fr>
2020-09-14 11:27:01 +0200

    python web

--- python web/td4/exo3.py
+++ python web/td4/exo3.py
@@ -1,19 +1,26 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 contenu = requests.get("https://stackoverflow.com/search?q=beautifulsoup")  
 soup = BeautifulSoup(contenu.text, "lxml")  
 questions = soup.find_all("div", class_="question-summary", limit=10)
+res = {}
+res['question'] = list()
+res['vote'] = list()
+res['rep'] = list()
 for q in questions :
     question = q.find_all("h3")
     vote = q.find("span", class_="vote-count-post")
     reponses = q.find("div", class_=(["status answered", "status answered-accepted"]))
     r = reponses.find("strong")
-    rep = list()
     for quest in question : 
-        res = list()
-        res.append(quest.text)
-        res.append(vote.text)
-        res.append(r.text)
-        rep.append(res)
-print(rep)
-        
+        res['question'].append(quest.text)
+        res['vote'].append(int(vote.text))
+        res['rep'].append(int(r.text))
+        #res.append((quest.text, int(vote.text), int(r.text)))
+print(res)
+df = pd.DataFrame(res, columns= ['Questions', 'Votes', 'Réponses'])
+
+df.to_csv (r'/home/fbazire/Bureau/LP/python web/td4', index = False, header=True)
+
+print (df)
